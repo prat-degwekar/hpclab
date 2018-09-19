5e1cf030341522d7a38d09ebeaeeeb656353acb3
prat-degwekar <prat.degwekar@gmail.com>
2018-09-19 16:47:14 +0530

    min working

--- mpi/min/mpiMin.c
+++ mpi/min/mpiMin.c
@@ -3,53 +3,56 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <limits.h>
 #define MAXSIZE 1000000
 
 int main(int argc, char **argv)
 {
-	int myid, numprocs;
-	int data[MAXSIZE], i, x, low, high, myresult=0, result;
-	char fn[255];
-	FILE *fp;
+        int myid, numprocs;
+        int i, x, low, high;
+        float data[MAXSIZE], result=0, myresult=0, mymin = INT_MAX, min = INT_MAX;
+        char fn[255];
+        FILE *fp;
 
-	MPI_Init(&argc, &argv);
-	MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
-	MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+        MPI_Init(&argc, &argv);
+        MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+        MPI_Comm_rank(MPI_COMM_WORLD, &myid);
 
-	if(0 == myid) {
-		/* open input file and intialize data */
-		strcpy(fn, getenv("PWD"));
-		strcat(fn, "/data.txt");
-		if( NULL == (fp = fopen(fn, "r")) ) {
-			printf("Can't open the input file: %s\n\n", fn);
-			exit(1);
-		}
-		for(i=0; i<MAXSIZE; i++) {
-			fscanf(fp, "%d", &data[i]);
-		}
-	}
+        if(0 == myid) {
+                /* open input file and intialize data */
+                strcpy(fn, getenv("PWD"));
+                strcat(fn, "/data.txt");
+                if( NULL == (fp = fopen(fn, "r")) ) {
+                        printf("Can't open the input file: %s\n\n", fn);
+                        exit(1);
+                }
+                for(i=0; i<MAXSIZE; i++) {
+                        fscanf(fp, "%f", &data[i]);
+                }
+        }
 
-	/* broadcast data */
-	MPI_Bcast(data, MAXSIZE, MPI_INT, 0, MPI_COMM_WORLD);
-	
-	/* add portion of data */
-	x = MAXSIZE/numprocs;	/* must be an integer */
-	low = myid * x;
-	high = low + x;
-	int min = INT_MAX;
-	for(i=low; i<high; i++) {
-		myresult += data[i];
-	}
-	printf("I got %d from %d\n", myresult, myid);
+        /* broadcast data */
+        MPI_Bcast(data, MAXSIZE, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
-	/* compute global sum */
-	MPI_Reduce(&myresult, &result, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+        /* add portion of data */
+        x = MAXSIZE/numprocs;   /* must be an integer */
+        low = myid * x;
+        high = low + x;
+        for(i=low; i<high; i++) {
+                //myresult += data[i];
+                if(data[i]<mymin)
+                {
+                    mymin = data[i];
+                }
+        }
+        printf("I got %f from %d\n", mymin, myid);
 
-	if(0 == myid) {
-		printf("The sum is %d.\n", result);
-	}
+        /* compute global sum */
+        MPI_Reduce(&mymin, &min, 1, MPI_FLOAT, MPI_MIN, 0, MPI_COMM_WORLD);
 
-	MPI_Finalize();
+        if(0 == myid) {
+                printf("The min is %f.\n", min);
+        }
+
+        MPI_Finalize();
 }
-
-	
